2404a6d4bf6ec02ed0a355c32669460b4e7e8b98
Andras Olah <andras.antal.olah@hu.ibm.com>
2021-02-26 10:54:44 +0100

    Add 3 more hearts, and a separator in css to position them

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ function App() {
     <HashRouter>
       <div className="App">
         <h1>Csak</h1>
-        <h2>úgy</h2>
+        <h2>úgy ma is :D</h2>
         <h2>Drukk! Drukk! Drukk!</h2>
-        <h3><img src={hearth} alt="Cute red hearth" className="App-logo" />
-        </h3>
+        <div className="hearts">
+        <img src={hearth} alt="Cute red hearth" className="App-logo" />
+        <img src={hearth} alt="Cute red hearth" className="App-logo" />
+        <img src={hearth} alt="Cute red hearth" className="App-logo" />
+        </div>
       </div>
     </HashRouter>
   );
